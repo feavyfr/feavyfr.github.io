1454b61804b6d32bdf9de40da2c6f540c640d3a6
Feavy <quentin071100@live.fr>
2022-11-01 19:01:24 +0100

    feat: add dotenv

--- source-notion/index.ts
+++ source-notion/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
 import { deleteArticle, generateArticle, getLocalArticles, getNotionArticles, hasUpdate } from "./utils/articles";
 
 (async () => {
